b82ed2a3a489f8853d61c1347ca3c3a7bcd8e910
Vajra <vinylscratch312@gmail.com>
2025-05-24 07:57:31 -0400

    Added pages

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const session = require('express-session');
 const crypto = require('crypto');
 
@@ -11,9 +12,12 @@ const generateSecretKey = () => {
   return crypto.randomBytes(32).toString('hex'); // Generates a 64-character hex string
 };
 
+const galleryDir = path.join(__dirname, 'public/images/dynamic');
+
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // serve static files like images
 
 // Session middleware
 app.use(
@@ -75,6 +79,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/access/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'access.html'));
+});
+
 app.get('/about/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
@@ -87,12 +95,36 @@ app.get('/contact-us/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'contact-us.html'));
 });
 
+app.get('/gallery/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'gallery.html'));
+});
+
+app.get('/news/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'news.html'));
+});
+
 app.get('/privacy-policy/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'privacy-policy.html'));
+  res.sendFile(path.join(__dirname, 'views', 'privacy.html'));
 });
 
 app.get('/services/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'services.html'));
+});
+
+app.get('/services/crop/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'crop.html'));
+});
+
+app.get('/services/retouch/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'retouch.html'));
+});
+
+app.get('/api/gallery-images', (req, res) => {
+  fs.readdir(galleryDir, (err, files) => {
+    if (err) return res.status(500).json({ error: 'Unable to read gallery directory' });
+    const images = files.filter(file => file.match(/\.(jpg|jpeg|png|webp)$/i));
+    res.json(images);
+  });
 });
 
 app.listen(port, () => {
